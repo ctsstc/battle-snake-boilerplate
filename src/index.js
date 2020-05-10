cfc63859d64487a47a34de1f783a6ae66a9c0501
Cody Swartz <ctsstc@gmail.com>
2020-05-10 03:51:10 -0700

    Add: Nodemon Remove: express-reload express reload did not work D:

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-const reload = require('express-reload');
 const port = 3000;
-const path = `${__dirname}/src/`
-
-app.use(reload(path));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
